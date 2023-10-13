7b593338a07d2a6a8ff5f5d576f579f72b343189
marvelfrozen <marvelfrozen@gmail.com>
2023-10-13 10:26:08 +0700

    change comments positions

--- src/gatling/kotlin/simulation/ReqResSimulation.kt
+++ src/gatling/kotlin/simulation/ReqResSimulation.kt
@@ -11,39 +11,45 @@ import java.time.Duration
 
 class ReqResSimulation : Simulation() {
   private val httpProtocol =
-      http.baseUrl( // variable to setup the gatling http protocol,
-          "https://reqres.in/") // set base url to reqres.in
+      // variable to setup the gatling http protocol, set base url to reqres.in
+      http.baseUrl("https://reqres.in/")
 
   private val getUserScn =
-      scenario("GetUserScenario") // the string parameter is actually not used in report
-          .group("get user scenario") // this is the one used in report to group up the requests
+      // the string parameter is actually not used in report
+      scenario("GetUserScenario")
+          // this is the one used to group up the requests in the report
+          .group("get user scenario")
           .on(
               exec(
-                      http("get user list") // set the request name
-                          .get("/api/users?page=2") // method and path of the api to test
-                          .check( // assert the response
-                              status().`is`(200), // for example, assert if the response code is 200
-                              jmesPath("data[0].id") // use jmespath to get the response body
-                                  .saveAs("userId") // and then save it to a variable
-                                  .name( // this is the error message if there's no user found
-                                      "no user found")))
+                      // set the request name
+                      http("get user list")
+                          // method and path of the api to test
+                          .get("/api/users?page=2")
+                          // asserts the response
+                          .check(
+                              // for example, asserts if the response code is 200
+                              status().`is`(200),
+                              // use jmespath to get the response body
+                              jmesPath("data[0].id")
+                                  // and then save it to a variable
+                                  .saveAs("userId")
+                                  // this is the error message if there's no user found
+                                  .name("no user found")))
                   .exec(
                       http("get user detail")
-                          .get(
-                              "/api/users/#{userId}") // use the saved variable from previous api
-                                                      // call here
+                          // use the saved variable from previous api call here using gatling's
+                          // expression laguage
+                          .get("/api/users/#{userId}")
                           .check(status().`is`(200))))
 
   private val createUserScn =
       scenario("CreateUserScenario")
           .exec(
               http("create user")
-                  .post("/api/users") // method and path of the api to test
-                  .body(
-                      StringBody(
-                          """{ "name": "gavril", "job": "quality" }""")) // we use StringBody() for
-                  // body request, but there
-                  // are other methods
+                  // method and path of the api to test
+                  .post("/api/users")
+                  // we use StringBody() for body request, but there are other methods
+                  .body(StringBody("""{ "name": "gavril", "job": "quality" }"""))
                   .check(status().`is`(201)))
 
   private val editUserScn =
@@ -58,11 +64,11 @@ class ReqResSimulation : Simulation() {
 
   init {
     setUp(
-            getUserScn.injectOpen( // set the scenarion that we want to run and the load model
-                constantUsersPerSec(1.0).during(Duration.ofSeconds(15))),
-            createUserScn.injectOpen(atOnceUsers(5)), // gatling support open and closed models
-            editUserScn.injectClosed( // read more https://gatling.io/docs/gatling/reference/current/core/injection/#open-vs-closed-workload-models
-                constantConcurrentUsers(2).during(10)))
+            // set the scenarion that we want to run and the load model
+            // gatling support open and closed models
+            getUserScn.injectOpen(constantUsersPerSec(1.0).during(Duration.ofSeconds(15))),
+            createUserScn.injectOpen(atOnceUsers(5)),
+            editUserScn.injectClosed(constantConcurrentUsers(2).during(10)))
         .protocols(httpProtocol)
   }
 }
